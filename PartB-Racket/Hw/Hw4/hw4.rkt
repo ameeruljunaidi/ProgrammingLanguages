7701eb4d0495c24aea184eade83550ebe2a97f67
AJ <ameerul.junaidi@gmail.com>
2022-03-16 11:42:25 -0400

    Completed cycle-lists

--- PartB-Racket/Hw/Hw4/hw4.rkt
+++ PartB-Racket/Hw/Hw4/hw4.rkt
@@ -81,3 +81,20 @@
   (letrec ([pr (s)]
            [f (lambda (x) (cons (cons 0 (car pr)) (lambda () (f (cdr pr)))))])
     (lambda () (f s))))
+
+
+; Problem 8
+; Write a function cycle-lists that takes two lists xs and ys and returns a stream. The lists may or
+; may not be the same length, but assume they are both non-empty. The elements produced by the
+; stream are pairs where the first part is from xs and the second part is from ys. The stream cycles
+; forever through the lists. For example, if xs is ’(1 2 3) and ys is ’("a" "b"), then the stream
+; would produce, (1 . "a"), (2 . "b"), (3 . "a"), (1 . "b"), (2 . "a"), (3 . "b"), (1 . "a"),
+; (2 . "b"), etc.
+; Sample solution is 6 lines and is more complicated than the previous stream problems. Hints: Use one
+; of the functions you wrote earlier. Use a recursive helper function that takes a number n and calls
+; itself with (+ n 1) inside a thunk
+
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (x)
+                (cons (cons (list-nth-mod xs x) (list-nth-mod ys x)) (lambda () (f (+ x 1)))))])
+    (lambda () (f 0))))
--- PartB-Racket/Hw/Hw4/hw4mytest.rkt
+++ PartB-Racket/Hw/Hw4/hw4mytest.rkt
@@ -25,6 +25,10 @@
    (check-equal? (stream-for-n-steps dan-then-dog 1) (list "dan.jpg") "dan-then-dog test")
 
    (check-equal? (stream-for-n-steps (stream-add-zero ones) 1) (list (cons 0 1)) "stream-add-zero test")
+
+   (check-equal? (stream-for-n-steps (cycle-lists (list 1 2 3) (list "a" "b")) 3) (list (cons 1 "a") (cons 2 "b") (cons 3 "a"))
+                 "cycle-lists test")
+
    ))
 
 (require rackunit/text-ui)
