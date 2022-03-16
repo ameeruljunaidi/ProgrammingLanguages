b449d98b83f765010ca97b875c2c92bbbc68a055
AJ <ameerul.junaidi@gmail.com>
2022-03-16 12:11:51 -0400

    Completed problem 9

--- PartB-Racket/Hw/Hw4/hw4.rkt
+++ PartB-Racket/Hw/Hw4/hw4.rkt
@@ -5,30 +5,17 @@
 ; put your code below
 
 ; Problem 1
-; Write a function sequence that takes 3 arguments low, high, and stride, all assumed to be numbers.
-; Further assume stride is positive. sequence produces a list of numbers from low to high (including
-; low and possibly high) separated by stride and in sorted order. Sample solution: 4 lines.
 
 (define (sequence lo hi stride)
   (if (> lo hi) null (cons lo (sequence (+ lo stride) hi stride))))
 
 
 ; Problem 2
-; Write a function string-append-map that takes a list of strings xs and a string suffix and returns a
-; list of strings. Each element of the output should be the corresponding element of the input appended
-; with suffix (with no extra space between the element and suffix). You must use Racket-library
-; functions map and string-append. Sample solution: 2 lines.
 
 (define (string-append-map xs suffix)
   (map (lambda (string) (string-append string suffix)) xs))
 
 ; Problem 3
-; Write a function list-nth-mod that takes a list xs and a number n. If the number is negative,
-; terminate the computation with (error "list-nth-mod: negative number"). Else if the list is
-; empty, terminate the computation with (error "list-nth-mod: empty list"). Else return the i-th
-; element of the list where we count from zero and i is the remainder produced when dividing n by the
-; list’s length. Library functions length, remainder, car, and list-tail are all useful – see the Racket
-; documentation. Sample solution is 6 lines.
 
 (define (list-nth-mod xs n)
   (cond [(< n 0) (error "list-nth-mod: negative number")]
@@ -36,9 +23,6 @@
         [#t (car (list-tail xs (remainder n (length xs))))]))
 
 ; Problem 4
-; Write a function stream-for-n-steps that takes a stream s and a number n. It returns a list holding
-; the first n values produced by s in order. Assume n is non-negative. Sample solution: 5 lines. Note:
-; You can test your streams with this function instead of the graphics code.
 
 (define (stream-for-n-steps s n)
   (if (= n 0)
@@ -47,10 +31,6 @@
         (cons (car pr) (stream-for-n-steps (cdr pr) (- n 1))))))
 
 ; Problem 5
-; Write a stream funny-number-stream that is like the stream of natural numbers (i.e., 1, 2, 3, ...)
-; except numbers divisble by 5 are negated (i.e., 1, 2, 3, 4, -5, 6, 7, 8, 9, -10, 11, ...). Remember a stream
-; is a thunk that when called produces a pair. Here the car of the pair will be a number and the cdr will
-; be another stream.
 
 (define funny-number-stream
   (letrec ([f (lambda (x)
@@ -60,10 +40,6 @@
     (lambda () (f 1))))
 
 ; Problem 6
-; Write a stream dan-then-dog, where the elements of the stream alternate between the strings "dan.jpg"
-; and "dog.jpg" (starting with "dan.jpg"). More specifically, dan-then-dog should be a thunk that
-; when called produces a pair of "dan.jpg" and a thunk that when called produces a pair of "dog.jpg"
-; and a thunk that when called... etc. Sample solution: 4 lines.
 
 (define dan-then-dog
   (letrec ([dan (lambda () (cons "dan.jpg" dog))]
@@ -71,11 +47,6 @@
     dan))
 
 ; Problem 7
-; Write a function stream-add-zero that takes a stream s and returns another stream. If s would
-; produce v for its i-th element, then (stream-add-zero s) would produce the pair (0 . v) for its i-th element.
-; Sample solution: 4 lines. Hint: Use a thunk that when called uses s and recursion.
-; Note: One of the provided tests in the file using graphics uses (stream-add-zero dan-then-dog)
-; with place-repeatedly.
 
 (define (stream-add-zero s)
   (letrec ([pr (s)]
@@ -84,17 +55,19 @@
 
 
 ; Problem 8
-; Write a function cycle-lists that takes two lists xs and ys and returns a stream. The lists may or
-; may not be the same length, but assume they are both non-empty. The elements produced by the
-; stream are pairs where the first part is from xs and the second part is from ys. The stream cycles
-; forever through the lists. For example, if xs is ’(1 2 3) and ys is ’("a" "b"), then the stream
-; would produce, (1 . "a"), (2 . "b"), (3 . "a"), (1 . "b"), (2 . "a"), (3 . "b"), (1 . "a"),
-; (2 . "b"), etc.
-; Sample solution is 6 lines and is more complicated than the previous stream problems. Hints: Use one
-; of the functions you wrote earlier. Use a recursive helper function that takes a number n and calls
-; itself with (+ n 1) inside a thunk
 
 (define (cycle-lists xs ys)
   (letrec ([f (lambda (x)
                 (cons (cons (list-nth-mod xs x) (list-nth-mod ys x)) (lambda () (f (+ x 1)))))])
     (lambda () (f 0))))
+
+; Problem 9
+
+(define (vector-assoc v vec)
+  (letrec ([find-v (lambda (i)
+                     (cond [(= i (- (vector-length vec) 1)) #f]
+                           [(equal? (car (vector-ref vec i)) v) (vector-ref vec i)]
+                           [#t (find-v (+ i 1))]))])
+    (find-v 0)))
+
+; Problem 10
